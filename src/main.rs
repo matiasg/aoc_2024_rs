a026f07b594b4303a62354a00c0a75738e0ea25b
Matias Graña <matias.alejo@gmail.com>
2025-01-21 10:53:34 -0300

    make main fancier

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ mod day_2;
 use std::env;
 
 fn main() {
+    let mains = [day_1::main, day_2::main];
+
     let args: Vec<String> = env::args().collect();
-    match args[1].as_str() {
-        "1" => day_1::main(),
-        "2" => day_2::main(),
-        &_ => todo!(),
-    }
+    let day: usize = args[1].parse().expect("can't understand day to run");
+    match day {
+        i if i > mains.len() || i < 1 => panic!("No day {day}"),
+        i => mains[i - 1](),
+    };
 }
